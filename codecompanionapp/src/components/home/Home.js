f2d15db154aac14fa100078c499f7d37623713d1
Moaz Zardab <moazzardab@gmail.com>
2024-10-29 22:47:52 +0000

    get started button and styling :( not enough work

--- codecompanionapp/src/components/home/Home.js
+++ codecompanionapp/src/components/home/Home.js
@@ -3,13 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import { useNavigate } from 'react-router-dom';
 import NavBar from '../common/Navbar';
+import { Button } from "react-bootstrap";
 
 const Home = () => {
+  const navigate = useNavigate(); // Initialize the navigation hook
+
+  const handleClick = () => {
+    navigate('/AI'); // Navigate to /AI when button is clicked
+  };
+  
   return (
-    <div class="d-flex justify-content-center">
-        <h1> This is the home page </h1>
+    <div className="d-flex justify-content-center mt-5">
+      <button class="button-start" onClick={handleClick}>Get Started!</button>
     </div>
   );
 }
